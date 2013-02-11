26c084de2243d1f0dd463e607acf15092c91861b
kuhjunge <quhberta@gmail.com>
2013-02-11 18:10:14 +0100

    - Startposition der Figuren direkt mit der Positions Klasse verknüpft

--- MAEDN/src/maednpackage/MAEDNWindow.java
+++ MAEDN/src/maednpackage/MAEDNWindow.java
@@ -42,7 +42,8 @@ public class MAEDNWindow {
 	 */
 	private void initialize() {
 		// Der Rahmen der das Gesammte Java Projekt umgibt
-		frmMenschaergereDich = new JFrame(); // Objekt wird deklariert & initalisiert
+		frmMenschaergereDich = new JFrame();
+		frmMenschaergereDich.setResizable(false);
 		frmMenschaergereDich.setTitle("Mensch \u00C4rgere Dich Nicht"); // Titel des Fensters
 		frmMenschaergereDich.getContentPane().setBackground(new Color(240, 230, 140)); // HintergrundOrange
 		frmMenschaergereDich.setBounds(100, 100, 600, 620); // Größe
@@ -59,61 +60,65 @@ public class MAEDNWindow {
 		ImageIcon imgGelb = new ImageIcon(this.getClass().getResource("images/maennchengelb.png")); //Rote Spielfigur
 		
 		// Figuren Gelb
-				final JLabel lblMYellow1 = new JLabel(imgGelb); // Blaue Spielfigur wird Bild zugewiesen
-				lblMYellow1.addMouseListener(new MouseAdapter() { // Klickevent
-					@Override
-					public void mouseClicked(MouseEvent arg0) { // Klickevent
-						int id = 1;
-						int farbe = 4;
-						int wurf = 1;
-						int i = maedn.zug(farbe,id,wurf);
-						lblMYellow1.setLocation(MADENPositions.place(farbe, id,i)) ;
-					}
-				});
-				lblMYellow1.setBounds(525, 470, 45, 45); // Startposition
-				frmMenschaergereDich.getContentPane().add(lblMYellow1); // Und Ferig, ab aufs Spielfeld mit dir
-				
-				final JLabel lblMYellow2 = new JLabel(imgGelb); // Blaue Spielfigur wird Bild zugewiesen
-				lblMYellow2.addMouseListener(new MouseAdapter() { // Klickevent
-					@Override
-					public void mouseClicked(MouseEvent arg0) { // Klickevent
-						int id = 2;
-						int farbe = 4;
-						int wurf = 1;
-						int i = maedn.zug(farbe,id,wurf);
-						lblMYellow2.setLocation(MADENPositions.place(farbe, id,i)) ;
-					}
-				});
-				lblMYellow2.setBounds(475, 528, 45, 45);
-				frmMenschaergereDich.getContentPane().add(lblMYellow2);
-				
-				final JLabel lblMYellow3 = new JLabel(imgGelb); // Blaue Spielfigur wird Bild zugewiesen
-				lblMYellow3.addMouseListener(new MouseAdapter() { // Klickevent
-					@Override
-					public void mouseClicked(MouseEvent arg0) { // Klickevent
-						int id = 3;
-						int farbe = 4;
-						int wurf = 1;
-						int i = maedn.zug(farbe,id,wurf);
-						lblMYellow3.setLocation(MADENPositions.place(farbe, id,i)) ;
-					}
-				});
-				lblMYellow3.setBounds(525, 528, 45, 45);
-				frmMenschaergereDich.getContentPane().add(lblMYellow3);
-				
-				final JLabel lblMYellow4 = new JLabel(imgGelb); // Blaue Spielfigur wird Bild zugewiesen
-				lblMYellow4.addMouseListener(new MouseAdapter() { // Klickevent
-					@Override
-					public void mouseClicked(MouseEvent arg0) { // Klickevent
-						int id = 4;
-						int farbe = 4;
-						int wurf = 1;
-						int i = maedn.zug(farbe,id,wurf);
-						lblMYellow4.setLocation(MADENPositions.place(farbe, id,i)) ;
-					}
-				});
-				lblMYellow4.setBounds(475, 470, 45, 45);
-				frmMenschaergereDich.getContentPane().add(lblMYellow4);
+		final JLabel lblMYellow1 = new JLabel(imgGelb); // Blaue Spielfigur wird Bild zugewiesen
+		lblMYellow1.addMouseListener(new MouseAdapter() { // Klickevent
+			@Override
+			public void mouseClicked(MouseEvent arg0) { // Klickevent
+				int id = 1;
+				int farbe = 4;
+				int wurf = 1;
+				int i = maedn.zug(farbe,id,wurf);
+				lblMYellow1.setLocation(MADENPositions.place(farbe, id,i)) ;
+			}
+		});
+		lblMYellow1.setSize(46, 46); // Größe
+		lblMYellow1.setLocation(MADENPositions.place(4, 1,0));// Position
+		frmMenschaergereDich.getContentPane().add(lblMYellow1); // Und Ferig, ab aufs Spielfeld mit dir
+		
+		final JLabel lblMYellow2 = new JLabel(imgGelb); // Blaue Spielfigur wird Bild zugewiesen
+		lblMYellow2.addMouseListener(new MouseAdapter() { // Klickevent
+			@Override
+			public void mouseClicked(MouseEvent arg0) { // Klickevent
+				int id = 2;
+				int farbe = 4;
+				int wurf = 1;
+				int i = maedn.zug(farbe,id,wurf);
+				lblMYellow2.setLocation(MADENPositions.place(farbe, id,i)) ;
+			}
+		});
+		lblMYellow2.setSize(46, 46); // Größe
+		lblMYellow2.setLocation(MADENPositions.place(4, 2,0));// Position
+		frmMenschaergereDich.getContentPane().add(lblMYellow2);
+		
+		final JLabel lblMYellow3 = new JLabel(imgGelb); // Blaue Spielfigur wird Bild zugewiesen
+		lblMYellow3.addMouseListener(new MouseAdapter() { // Klickevent
+			@Override
+			public void mouseClicked(MouseEvent arg0) { // Klickevent
+				int id = 3;
+				int farbe = 4;
+				int wurf = 1;
+				int i = maedn.zug(farbe,id,wurf);
+				lblMYellow3.setLocation(MADENPositions.place(farbe, id,i)) ;
+			}
+		});
+		lblMYellow3.setSize(46, 46); // Größe
+		lblMYellow3.setLocation(MADENPositions.place(4, 3,0));// Position
+		frmMenschaergereDich.getContentPane().add(lblMYellow3);
+		
+		final JLabel lblMYellow4 = new JLabel(imgGelb); // Blaue Spielfigur wird Bild zugewiesen
+		lblMYellow4.addMouseListener(new MouseAdapter() { // Klickevent
+			@Override
+			public void mouseClicked(MouseEvent arg0) { // Klickevent
+				int id = 4;
+				int farbe = 4;
+				int wurf = 1;
+				int i = maedn.zug(farbe,id,wurf);
+				lblMYellow4.setLocation(MADENPositions.place(farbe, id,i)) ;
+			}
+		});
+		lblMYellow4.setSize(46, 46); // Größe
+		lblMYellow4.setLocation(MADENPositions.place(4, 4,0));// Position
+		frmMenschaergereDich.getContentPane().add(lblMYellow4);
 		
 		// Figuren Grün
 		final JLabel lblMGreen1 = new JLabel(imgGruen); // Blaue Spielfigur wird Bild zugewiesen
@@ -127,7 +132,8 @@ public class MAEDNWindow {
 				lblMGreen1.setLocation(MADENPositions.place(farbe, id,i)) ;
 			}
 		});
-		lblMGreen1.setBounds(525, 10, 45, 45); // Startposition
+		lblMGreen1.setSize(46, 46); // Größe
+		lblMGreen1.setLocation(MADENPositions.place(3, 1,0));// Position
 		frmMenschaergereDich.getContentPane().add(lblMGreen1); // Und Ferig, ab aufs Spielfeld mit dir
 		
 		final JLabel lblMGreen2 = new JLabel(imgGruen); // Blaue Spielfigur wird Bild zugewiesen
@@ -141,7 +147,8 @@ public class MAEDNWindow {
 				lblMGreen2.setLocation(MADENPositions.place(farbe, id,i)) ;
 			}
 		});
-		lblMGreen2.setBounds(475, 60, 45, 45);
+		lblMGreen2.setSize(46, 46); // Größe
+		lblMGreen2.setLocation(MADENPositions.place(3, 2,0));// Position
 		frmMenschaergereDich.getContentPane().add(lblMGreen2);
 		
 		final JLabel lblMGreen3 = new JLabel(imgGruen); // Blaue Spielfigur wird Bild zugewiesen
@@ -155,7 +162,8 @@ public class MAEDNWindow {
 				lblMGreen3.setLocation(MADENPositions.place(farbe, id,i)) ;
 			}
 		});
-		lblMGreen3.setBounds(525, 60, 45, 45);
+		lblMGreen3.setSize(46, 46); // Größe
+		lblMGreen3.setLocation(MADENPositions.place(3, 3,0));// Position
 		frmMenschaergereDich.getContentPane().add(lblMGreen3);
 		
 		final JLabel lblMGreen4 = new JLabel(imgGruen); // Blaue Spielfigur wird Bild zugewiesen
@@ -169,7 +177,8 @@ public class MAEDNWindow {
 				lblMGreen4.setLocation(MADENPositions.place(farbe, id,i)) ;
 			}
 		});
-		lblMGreen4.setBounds(475, 10, 45, 45);
+		lblMGreen4.setSize(46, 46); // Größe
+		lblMGreen4.setLocation(MADENPositions.place(3, 4,0));// Position
 		frmMenschaergereDich.getContentPane().add(lblMGreen4);
 		
 		// Figuren Rot
@@ -184,7 +193,8 @@ public class MAEDNWindow {
 				lblMRed1.setLocation(MADENPositions.place(farbe, id,i)) ;
 			}
 		});
-		lblMRed1.setBounds(62, 470, 45, 45); // Startposition
+		lblMRed1.setSize(46, 46); // Größe
+		lblMRed1.setLocation(MADENPositions.place(1, 1,0));// Position
 		frmMenschaergereDich.getContentPane().add(lblMRed1); // Und Ferig, ab aufs Spielfeld mit dir
 		
 		final JLabel lblMRed2 = new JLabel(imgRot); // Blaue Spielfigur wird Bild zugewiesen
@@ -198,7 +208,8 @@ public class MAEDNWindow {
 				lblMRed2.setLocation(MADENPositions.place(farbe, id,i)) ;
 			}
 		});
-		lblMRed2.setBounds(10, 528, 45, 45);
+		lblMRed2.setSize(46, 46); // Größe
+		lblMRed2.setLocation(MADENPositions.place(1, 2,0));// Position
 		frmMenschaergereDich.getContentPane().add(lblMRed2);
 		
 		final JLabel lblMRed3 = new JLabel(imgRot); // Blaue Spielfigur wird Bild zugewiesen
@@ -212,7 +223,8 @@ public class MAEDNWindow {
 				lblMRed3.setLocation(MADENPositions.place(farbe, id,i)) ;
 			}
 		});
-		lblMRed3.setBounds(62, 528, 45, 45);
+		lblMRed3.setSize(46, 46); // Größe
+		lblMRed3.setLocation(MADENPositions.place(1, 3,0));// Position
 		frmMenschaergereDich.getContentPane().add(lblMRed3);
 		
 		final JLabel lblMRed4 = new JLabel(imgRot); // Blaue Spielfigur wird Bild zugewiesen
@@ -226,7 +238,8 @@ public class MAEDNWindow {
 				lblMRed4.setLocation(MADENPositions.place(farbe, id,i)) ;
 			}
 		});
-		lblMRed4.setBounds(10, 470, 45, 45);
+		lblMRed4.setSize(46, 46); // Größe
+		lblMRed4.setLocation(MADENPositions.place(1, 4,0));// Position
 		frmMenschaergereDich.getContentPane().add(lblMRed4);
 		
 		// Figuren Blau
@@ -241,7 +254,8 @@ public class MAEDNWindow {
 				lblMBlue1.setLocation(MADENPositions.place(farbe, id,i)) ;
 			}
 		});
-		lblMBlue1.setBounds(62, 10, 45, 45); // Startposition
+		lblMBlue1.setSize(46, 46); // Größe
+		lblMBlue1.setLocation(MADENPositions.place(2, 1,0));// Position
 		frmMenschaergereDich.getContentPane().add(lblMBlue1); // Und Ferig, ab aufs Spielfeld mit dir
 		
 		final JLabel lblMBlue2 = new JLabel(imgBlau); // Blaue Spielfigur wird Bild zugewiesen
@@ -255,7 +269,8 @@ public class MAEDNWindow {
 				lblMBlue2.setLocation(MADENPositions.place(farbe, id,i)) ;
 			}
 		});
-		lblMBlue2.setBounds(10, 60, 45, 45);
+		lblMBlue2.setSize(46, 46);// Größe
+		lblMBlue2.setLocation(MADENPositions.place(2, 2,0)); // Position
 		frmMenschaergereDich.getContentPane().add(lblMBlue2);
 		
 		final JLabel lblMBlue3 = new JLabel(imgBlau); // Blaue Spielfigur wird Bild zugewiesen
@@ -269,7 +284,8 @@ public class MAEDNWindow {
 				lblMBlue3.setLocation(MADENPositions.place(farbe, id,i)) ;
 			}
 		});
-		lblMBlue3.setBounds(62, 60, 45, 45);
+		lblMBlue3.setSize(46, 46);// Größe
+		lblMBlue3.setLocation(MADENPositions.place(2, 3,0)); // Position
 		frmMenschaergereDich.getContentPane().add(lblMBlue3);
 		
 		final JLabel lblMBlue4 = new JLabel(imgBlau); // Blaue Spielfigur wird Bild zugewiesen
@@ -283,30 +299,13 @@ public class MAEDNWindow {
 				lblMBlue4.setLocation(MADENPositions.place(farbe, id,i)) ;
 			}
 		});
-		lblMBlue4.setBounds(10, 10, 45, 45);
+		lblMBlue4.setSize(46, 46);// Größe
+		lblMBlue4.setLocation(MADENPositions.place(2, 4,0)); // Position
 		frmMenschaergereDich.getContentPane().add(lblMBlue4);
 		
 		// Zum Schluss den Hintergrund laden
 		JLabel lblBackground = new JLabel(imgBackground); // Backgroundimage Zuweisen
 		lblBackground.setBounds(0, 0, 580, 580); // Größe
 		frmMenschaergereDich.getContentPane().add(lblBackground); // An den Frame anheften
-		
-		// Aufräumen
-		lblMRed1.setLocation(MADENPositions.place(1, 1,0));
-		lblMRed2.setLocation(MADENPositions.place(1, 2,0));
-		lblMRed3.setLocation(MADENPositions.place(1, 3,0));
-		lblMRed4.setLocation(MADENPositions.place(1, 4,0));
-		lblMBlue1.setLocation(MADENPositions.place(2, 1,0));
-		lblMBlue2.setLocation(MADENPositions.place(2, 2,0));
-		lblMBlue3.setLocation(MADENPositions.place(2, 3,0));
-		lblMBlue4.setLocation(MADENPositions.place(2, 4,0));
-		lblMGreen1.setLocation(MADENPositions.place(3, 1,0));
-		lblMGreen2.setLocation(MADENPositions.place(3, 2,0));
-		lblMGreen3.setLocation(MADENPositions.place(3, 3,0));
-		lblMGreen4.setLocation(MADENPositions.place(3, 4,0));
-		lblMYellow1.setLocation(MADENPositions.place(4, 1,0));
-		lblMYellow2.setLocation(MADENPositions.place(4, 2,0));
-		lblMYellow3.setLocation(MADENPositions.place(4, 3,0));
-		lblMYellow4.setLocation(MADENPositions.place(4, 4,0));
 	}
 }
